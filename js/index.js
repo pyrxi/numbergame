57b372a23fa329e9f7c55a3b0a6147b584f6ded8
pyrxi <pyrxin@proton.me>
2023-07-23 22:34:09 -0400

    ??[5]

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ let mod1   = 0;
 
 //increment modifier
 let increment = 1;
-
+let respond;
 
 function numberGoUp () {
   let newnumber = ((number*10) + (increment*10))/10;
@@ -12,7 +12,7 @@ function numberGoUp () {
   document.getElementById('thenumber').innerHTML = number;
 }
 
-function modOneGoUp () {
+function modOneUp1() {
   if (number >= 2) {
   mod1++;
   increment = ((mod1*9) * (0.2*10)/10);
@@ -22,4 +22,32 @@ function modOneGoUp () {
   document.getElementById('modifier1').innerHTML = mod1;
   document.getElementById('incremental').innerHTML = increment;
   }
+}
+
+function modOneUp10() {
+  if (number >= 20) {
+  mod1 = mod1 + 10;
+  increment = ((mod1*9) * (0.2*10)/10);  
+  number = number - 20;
+  printNumber= number.toPrecision(2);  
+  document.getElementById('thenumber').innerHTML = number;
+  document.getElementById('modifier1').innerHTML = mod1;  
+  document.getElementById('incremental').innerHTML = increment;
+  }
+}
+
+function modOneUp100() {
+  if (number >= 200) {
+  mod1 = mod1 + 100;  
+  increment = ((mod1*9) * (0.2*10)/10);  
+  number = number - 200;  
+  printNumber= number.toPrecision(2);  
+  document.getElementById('thenumber').innerHTML = number;
+  document.getElementById('modifier1').innerHTML = mod1;  
+  document.getElementById('incremental').innerHTML = increment;
+  }
+}
+
+function submitOnPress() {
+  document.getElementById('response').innerHTML = "no";
 }
