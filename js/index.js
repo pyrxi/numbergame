813f2dfe56596587e29c34ac12cfe11e1c742866
pyrxi <pyrxin@proton.me>
2024-07-05 14:02:36 -0400

    refactor: redo the entire codebase

--- js/index.js
+++ js/index.js
@@ -1,53 +1,37 @@
-//number + modifiers
-let number = 0;
-let mod1   = 0;
-
-//increment modifier
+// number + modifiers
+let main_number = 0;
+let mod1 = 0;
 let increment = 1;
-let respond;
+
+// UI Elements
+let number_display = document.getElementById('thenumber');
+let modifier1_display = document.getElementById('modifier1');
+let incremental_display = document.getElementById('incremental');
+let response_display = document.getElementById('response');
 
 function numberGoUp () {
-  let newnumber = ((number*10) + (increment*10))/10;
-  number = newnumber;
-  document.getElementById('thenumber').innerHTML = number;
+  // reassigns the value of the number to the incremented amount
+  main_number = ((main_number*10) + (increment*10))/10;
+  // updates the ui
+  number_display.innerHTML = main_number;
 }
 
-function modOneUp1() {
-  if (number >= 2) {
-  mod1++;
-  increment = ((mod1*9) * (0.2*10)/10);
-  number = number - 2;
-  printNumber= number.toPrecision(2);
-  document.getElementById('thenumber').innerHTML = number;
-  document.getElementById('modifier1').innerHTML = mod1;
-  document.getElementById('incremental').innerHTML = increment;
-  }
-}
-
-function modOneUp10() {
-  if (number >= 20) {
-  mod1 = mod1 + 10;
-  increment = ((mod1*9) * (0.2*10)/10);  
-  number = number - 20;
-  printNumber= number.toPrecision(2);  
-  document.getElementById('thenumber').innerHTML = number;
-  document.getElementById('modifier1').innerHTML = mod1;  
-  document.getElementById('incremental').innerHTML = increment;
-  }
-}
-
-function modOneUp100() {
-  if (number >= 200) {
-  mod1 = mod1 + 100;  
-  increment = ((mod1*9) * (0.2*10)/10);  
-  number = number - 200;  
-  printNumber= number.toPrecision(2);  
-  document.getElementById('thenumber').innerHTML = number;
-  document.getElementById('modifier1').innerHTML = mod1;  
-  document.getElementById('incremental').innerHTML = increment;
+function modOneUp(amount, price) {
+  // if the main number is high enough
+  if (main_number >= price) {
+    // increment mod1
+    mod1 = mod1 + amount;
+    // add modifier amount to total increment
+    increment = ((mod1*9) * (0.2*10)/10);
+    // subtract main number by modifier1 cost
+    main_number = main_number - price;
+    // update display
+    number_display.innerHTML = main_number.toPrecision(2);
+    modifier1_display.innerHTML = mod1;
+    incremental_display.innerHTML = increment;
   }
 }
 
 function submitOnPress() {
-  document.getElementById('response').innerHTML = "no";
+  response_display.innerHTML = "no";
 }
